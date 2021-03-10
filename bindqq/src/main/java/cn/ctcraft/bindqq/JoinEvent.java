3a4df75a7b27375491d30a09a4ea07d92dfe3b57
yneit <54214328+yneit@users.noreply.github.com>
2021-03-10 11:28:33 +0800

    异步获取 qq 名

--- bindqq/src/main/java/cn/ctcraft/bindqq/JoinEvent.java
+++ bindqq/src/main/java/cn/ctcraft/bindqq/JoinEvent.java
@@ -1,13 +1,10 @@
 package cn.ctcraft.bindqq;
 
 import cn.ctcraft.bindqq.database.Database;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
 public class JoinEvent implements Listener {
     Database database;
